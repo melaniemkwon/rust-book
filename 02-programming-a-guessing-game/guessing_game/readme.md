026fc5661f2e5c0a672d0c4ba47b9c84b427a26b
melaniemkwon <melaniemkwon@gmail.com>
2019-12-14 05:46:34 -0800

    rand num gen

--- 02-programming-a-guessing-game/guessing_game/readme.md
+++ 02-programming-a-guessing-game/guessing_game/readme.md
@@ -1,1 +1,11 @@
 https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+
+https://crates.io
+
+https://doc.rust-lang.org/cargo/reference/publishing.html
+
+### Build documentation for dependencies locally
+```
+cargo doc --open
+```
+02-programming-a-guessing-game/guessing_game/target/doc/rand/trait.Rng.html#method.gen_range
--- 02-programming-a-guessing-game/guessing_game/src/main.rs
+++ 02-programming-a-guessing-game/guessing_game/src/main.rs
@@ -1,13 +1,24 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    // rand::thread_rng
+    //   gives us the random number generator
+    //   that is local to the current thread of execution
+    //   and seeded by the operating system
+    // gen_range
+    //   method is defined by the Rng trait that we brought
+    //   into scope with use rand::Rng
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
+    println!("Please input your guess.");
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
 }
